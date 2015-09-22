a5f0165f76dc14c480c0784ed76d5349ef22c867
Brent Shaffer <bshafs@gmail.com>
2015-09-22 11:29:38 -0700

    adds config to container, adds openID container test

---
+++ Tests/ContainerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace OAuth2\ServerBundle\Tests;
+
+use OAuth2\Request;
+use OAuth2\Response;
+use OAuth2\ServerBundle\Tests\ContainerLoader;
+use OAuth2\ServerBundle\Command\CreateClientCommand;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOpenIdConfig()
+    {
+        $openIdConfig = sprintf(<<<EOF
+<?xml version="1.0"?>
+<container xmlns="http://symfony.com/schema/dic/services" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://symfony.com/schema/dic/services http://symfony.com/schema/dic/services/services-1.0.xsd">
+    <imports>
+        <import resource="%s" />
+    </imports>
+    <parameters>
+        <parameter key="oauth2.server.config" type="collection">
+            <parameter key="use_openid_connect">true</parameter>
+            <parameter key="issuer">oauth2-server-bundle</parameter>
+        </parameter>
+    </parameters>
+</container>
+EOF
+, __DIR__.'/../vendor/symfony/symfony/src/Symfony/Bundle/SecurityBundle/Resources/config/security.xml');
+        file_put_contents($tmpFile = tempnam(sys_get_temp_dir(), 'openid-config'), $openIdConfig);
+
+        $container = ContainerLoader::buildTestContainer(array($tmpFile));
+
+        $config = $container->getParameter('oauth2.server.config');
+        $server = $container->get('oauth2.server');
+
+        $this->assertTrue($server->getConfig('use_openid_connect'));
+        $this->assertNotNull($server->getStorage('public_key'));
+
+        $clientId = 'test-client-' . rand();
+
+        $server->getStorage('public_key')->keys['public_key'] = file_get_contents(__DIR__.'/../vendor/bshaffer/oauth2-server-php/test/config/keys/id_rsa.pub');
+        $server->getStorage('public_key')->keys['private_key'] = file_get_contents(__DIR__.'/../vendor/bshaffer/oauth2-server-php/test/config/keys/id_rsa');
+        $server->getStorage('client_credentials')->setClientDetails($clientId, 'test-client-secret');
+        $request = new Request(array(
+            'client_id'     => $clientId,
+            'redirect_uri'  => 'http://brentertainment.com',
+            'response_type' => 'code',
+            'scope'         => 'openid',
+            'state'         => 'xyz',
+        ));
+
+        $response = new Response();
+        $server->handleAuthorizeRequest($request, $response, true);
+        $parts = parse_url($response->getHttpHeader('Location'));
+        parse_str($parts['query'], $query);
+        $code = $server->getStorage('authorization_code')->getAuthorizationCode($query['code']);
+
+        $this->assertArrayHasKey('id_token', $code);
+    }
+}
